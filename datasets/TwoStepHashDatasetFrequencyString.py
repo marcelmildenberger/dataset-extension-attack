21a2cbf0eb7704c297568217c7824a316141c884
Marcel Mildenberger <marcel.mildenberger@sap.com>
2025-03-04 15:48:58 +0100

    updated namings

--- datasets/TwoStepHashDatasetFrequencyString.py
+++ datasets/TwoStepHashDatasetFrequencyString.py
@@ -2,41 +2,34 @@ import numpy as np
 import torch
 from torch.utils.data import Dataset
 
-class TwoStepHashDataset(Dataset):
+class TwoStepHashDatasetFrequencyString(Dataset):
     def __init__(self, data, isLabeled=False, all_two_grams=None, max_length=None):
         self.isLabeled = isLabeled
         self.allTwoGrams = all_two_grams
         self.data = data
         self.max_length = max_length
+        self.hash_tensors = self.data['twostephash'].apply(lambda row: self.hash_list_to_tensor(list(row)))
 
         if self.isLabeled:
             # For reidentified data, extract labels (2-grams) from values except last two columns
             self.data['label'] = self.data.apply(lambda row: self.extract_two_grams("".join(row.iloc[:-2].astype(str))), axis=1)
+            self.label_tensors = self.data['label'].apply(lambda row: self.label_to_tensor(row))
 
     def __len__(self):
         return len(self.data)
 
     def __getitem__(self, idx):
-        tsh_encoding = self.data.iloc[idx]['twostephash']
-        # Parse the string (e.g., "{273928, 1785355, ...}") into a list of integers
-        hash_tensor = self.hash_list_to_tensor(list(tsh_encoding))
-
         if self.isLabeled:
-            label = self.data.iloc[idx]['label']
-            label_tensor = self.label_to_tensor(label)
-            return hash_tensor, label_tensor
+            return self.hash_tensors[idx], self.label_tensors[idx]
         else:
-            return hash_tensor
+            return self.hash_tensors[idx]
 
     def hash_list_to_tensor(self, hash_list):
-        hash_array = np.array(hash_list, dtype=np.float32)
-        if self.max_length is not None:
-            if len(hash_array) < self.max_length:
-                pad_width = self.max_length - len(hash_array)
-                hash_array = np.pad(hash_array, (0, pad_width), mode='constant', constant_values=0)
-            else:
-                hash_array = hash_array[:self.max_length]
-        return torch.tensor(hash_array)
+        hash_array = [int(entry) for entry in hash_list]
+        hash_tensor = np.zeros(self.max_length, dtype=np.float32)
+        for val in hash_array:
+            hash_tensor[val-1] = hash_tensor[val-1] + 1
+        return torch.tensor(hash_tensor)
 
     def extract_two_grams(self, input_string):
         input_string_preprocessed = input_string.replace('"', '').replace('.', '').replace('/', '').strip()
