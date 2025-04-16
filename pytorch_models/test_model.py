c4a34e62899d959c1a374d361391aa1e78f1dfe8
Marcel Mildenberger <marcel.mildenberger@sap.com>
2025-04-16 15:12:50 +0200

    small improvements

--- pytorch_models/test_model.py
+++ pytorch_models/test_model.py
@@ -1,19 +1,25 @@
 import torch.nn as nn
 
+activation_functions = {
+    "relu": nn.ReLU(),
+    "leaky_relu": nn.LeakyReLU(),
+    "gelu": nn.GELU(),
+}
+
 class TestModel(nn.Module):
-    def __init__(self, input_dim, output_dim, hidden_layer=256, num_layers=4, dropout_rate=0.2):
+    def __init__(self, input_dim, output_dim, hidden_layer=256, num_layers=4, dropout_rate=0.2, activation_fn="relu"):
         super(TestModel, self).__init__()
 
         # Define the layers for multi-label classification of 2-grams
         layers = [
             nn.Linear(input_dim, hidden_layer),  # Input layer to first hidden layer
-            nn.ReLU(),  # Activation function
+            activation_functions[activation_fn],  # Activation function
             nn.Dropout(dropout_rate),
         ]
 
         for _ in range(num_layers - 1):  # Add dynamic hidden layers
             layers.append(nn.Linear(hidden_layer, hidden_layer))  # Hidden layer to next hidden layer
-            layers.append(nn.ReLU())  # Activation function
+            layers.append(activation_functions[activation_fn])  # Activation function
             layers.append(nn.Dropout(dropout_rate))  # Dropout layer
 
         layers.append(nn.Linear(hidden_layer, output_dim))  # Output layer
